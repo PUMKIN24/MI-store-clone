6fce5b478f0ba32fe43f408a0ad9da59c700295f
PUMKIN24 <jer2ymatheww@gmail.com>
2022-09-16 19:15:01 -0700

    4th commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
 import Slider from './components/Slider';
-import {BrowserRouter as Router } from 'react-router-dom'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 import data from '../src/data/data.json'
 import Offers from './components/Offers';
 import Heading from './components/Heading';
 import StarProducts from './components/StarProducts'
 import HotAccessoriesMenu from './components/HotAccessoriesMenu';
+import HotAccessories from './components/HotAccessories';
+
 
 
 function App() {
@@ -19,8 +21,12 @@ function App() {
     <Offers offer={data.offer} />
     <Heading text="STAR PRODUCTS" />
     <StarProducts starProduct={data.starProduct}/> 
-    <Heading text="HOT ACCESSORIES" />
-    <HotAccessoriesMenu />
+    <Heading text="HAPPY SHOPPING" />
+    {/* <HotAccessoriesMenu /> */}
+
+      {/* <Route exact path='/music'>
+        <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music} />
+      </Route> */}
   </Router>
   );
 }
